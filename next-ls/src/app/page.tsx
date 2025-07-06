9a4295e1c82de7c158de830f2c3da7d2b48888e7
Volodymyr Kovalenko <dante77758@gmail.com>
2025-07-06 19:57:39 +0300

    add tailwind css

--- next-ls/src/app/page.tsx
+++ next-ls/src/app/page.tsx
@@ -1,7 +1,20 @@
+import StatusLabel, { Status } from '@/app/components/status-label';
+import NotActiveLabel from './components/not-active-label';
+import ActiveLabel from './components/active-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Notactive</NotActiveLabel>
+      <div>
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      </div>
+      <p className="text-orange-700 bg-orange-100">Status</p>
     </main>
   );
 }
